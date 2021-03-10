021c7de790068753e36f26e7d5ab630f2fa0467d
Anwesh43 <anweshthecool0@gmail.com>
2021-03-10 21:13:47 +0530

    adding SILNode clas

--- index.ts
+++ index.ts
@@ -123,3 +123,45 @@ class Animator {
         }
     }
 }
+
+class SILNode {
+
+    prev : SILNode 
+    next : SILNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SILNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSILNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SILNode {
+        var curr : SILNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
