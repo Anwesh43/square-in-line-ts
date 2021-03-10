fcd61f01ab32ce6d3b73c83afac262e638e857d0
Anwesh43 <anweshthecool0@gmail.com>
2021-03-10 21:22:24 +0530

    adding SquareInLine class

--- index.ts
+++ index.ts
@@ -165,3 +165,26 @@ class SILNode {
         return this 
     }
 }
+
+class SquareInLine {
+
+    curr : SILNode = new SILNode(0)
+    dir : number = 1 
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
